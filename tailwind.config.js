3e4d37beec04cdb3c0e5f5740c1403606b10501e
Hanntuu <raihanrrdarmawan@gmail.com>
2024-11-26 22:12:37 +0700

    setup project boring

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./App.{js,jsx,ts,tsx}", "/src/**.{js,ksx,ts,tsx}"],
+  theme: {},
+  plugin: [],
+};
